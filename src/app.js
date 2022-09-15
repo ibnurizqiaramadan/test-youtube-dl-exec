9dc391c3cc4bd20262f7e6b95fa9b83be283c93d
ibnurizqiaramadan <dadanibnu61@gmail.com>
2022-09-15 21:31:26 +0700

    using all interface

--- src/app.js
+++ src/app.js
@@ -136,6 +136,6 @@ app.get("/play-video-test.mp4", async (req, res) => {
         });
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`App listening on port ${port}`);
 });
